3e363393f0cf5e25bbe2ecc114f3731af856bf59
pradeepsutharr <psuthar318@gmail.com>
2024-06-03 09:11:54 +0530

    data

--- first-app/src/Components/ModernInterior.jsx
+++ first-app/src/Components/ModernInterior.jsx
@@ -1,27 +1,56 @@
-import React from 'react'
+import React from "react";
 
 function ModernInterior() {
-    return (
-        <section>
-            <div className="container mx-auto">
-                <div className="row">
-                    <div className="md:w-7/12 w-full">
-                        <div className="image-grid justify-between grid grid-cols-12 grid-row-12 relative">
-                            <div className="relative grid  col-start-1 col-end-8 row-start-1 row-end-7"> <img className='' src="images/img-grid-1.jpg" alt="" /></div>
-                            <div className="relative grid ps-4 col-start-8 col-end-12 row-start-1 row-end-6"><img className='' src="images/img-grid-2.jpg" alt="" /></div>
-                            <div className="relative grid col-start-6 col-end-12 row-start-6 row-end-12"><img className='' src="images/img-grid-3.jpg" alt="" /></div>
-
-
-
-                        </div>
-                    </div>
-
-                    <div className="5/12"></div>
-                </div>
+  return (
+    <section className="py-20">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap justify-between">
+          <div className="md:w-7/12 w-full ">
+            <div className="image-grid justify-between grid grid-cols-12 grid-row-13 relative">
+              <div className="relative grid  col-start-1 col-end-9 row-start-1 row-end-8">
+                {" "}
+                <img
+                  className="rounded-xl"
+                  src="images/img-grid-1.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="relative grid ps-4 col-start-9 col-end-13 row-start-1 row-end-6">
+                <img
+                  className="rounded-xl"
+                  src="images/img-grid-2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="relative grid pt-4 col-start-7 col-end-13 row-start-6 row-end-13">
+                <img
+                  className="rounded-xl"
+                  src="images/img-grid-3.jpg"
+                  alt=""
+                />
+              </div>
             </div>
+          </div>
 
-        </section>
-    )
+          <div className="md:w-5/12 w-full ps-5 ">
+            <div classname="content">
+              <h2 className="text-4xl text-gray-800 font-medium ">
+                We Help You Make Modern Interior Design
+              </h2>
+              <p className="text-gray-400 leading-7">
+                Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio
+                quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
+                vulputate velit imperdiet dolor tempor tristique. Pellentesque
+                habitant morbi tristique senectus et netus et malesuada
+              </p>
+
+              <ul className=""></ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default ModernInterior
+export default ModernInterior;
