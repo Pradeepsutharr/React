2dc7a066dd73dacd0796851abc80416bb02fa4ca
pradeepsutharr <psuthar318@gmail.com>
2024-06-05 22:12:57 +0530

    data

--- 09-theme-switcher/src/App.js
+++ 09-theme-switcher/src/App.js
@@ -1,20 +1,39 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./Components/Card";
+import ThemeBtn from "./Components/ThemeBtn";
+import { ThemeProvider } from "./Context/Theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
 
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* theme btn */}
-        </div>
-
-        <div className="w-full max-w-sm mx-auto">
-          {/* card */}
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
-    </div>
-
+    </ThemeProvider>
   );
 }
 
--- 09-theme-switcher/src/Context/Theme.js
+++ 09-theme-switcher/src/Context/Theme.js
@@ -1,9 +1,14 @@
-import React, { createContext } from 'react'
-import { createContext, useContext } from 'react'
+// import React, { createContext } from "react";
+import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
-    themeMode: "Light",
-    darkTheme: ()=>{},
-    lightTheme: ()=>{}
-})
+  themeMode: "Light",
+  darkTheme: () => {},
+  lightTheme: () => {},
+});
 
+export const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme() {
+  return useContext(ThemeContext);
+}
